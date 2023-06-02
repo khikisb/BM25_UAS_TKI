9e049c156a92ae87a2b3234145de9d96c8073c70
khikisb <89850965+khikisb@users.noreply.github.com>
2023-06-02 20:23:37 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,30 +3,6 @@ from parse import QueryParser, CorpusParser
 from query import QueryProcessor
 import operator
 
-def run_query(queries, corpus):
-    # Inisialisasi QueryProcessor dengan queries dan corpus
-    proc = QueryProcessor(queries, corpus)
-
-    # Menjalankan proses pencarian query
-    results = proc.run()
-
-    qid = 0  # ID untuk query
-    for result in results:
-        # Mengurutkan hasil pencarian berdasarkan nilai (dalam urutan menurun)
-        sorted_x = sorted(result.items(), key=operator.itemgetter(1))
-        sorted_x.reverse()
-
-        index = 1  # Index untuk ranking hasil pencarian
-
-        # Mengambil 100 hasil pencarian teratas
-        for i in sorted_x[:100]:
-            tmp = (qid, i[0], index, i[1])
-            # Menampilkan hasil pencarian dengan format yang ditentukan
-            st.write('{:>1}\tQ0\t{:>4}\t{:>2}\t{:>12}\tNH-BM25'.format(*tmp))
-            index += 1
-
-        qid += 1  # Menaikkan ID query untuk query selanjutnya
-
 class QueryParser:
     def __init__(self):
         self.queries = []
@@ -51,11 +27,42 @@ def main():
     # Initialize and parse queries using QueryParser
     qp = QueryParser()
     qp.parse(queries)
-
+    
     # Retrieve and display the parsed queries
     parsed_queries = qp.get_queries()
     for query in parsed_queries:
         st.write(query)
+        
+    # Parsing corpus dari file corpus.txt
+    cp.parse()
+    # Mendapatkan daftar dokumen dalam corpus
+    corpus = cp.get_corpus()
+    # Inisialisasi QueryProcessor dengan queries dan corpus
+    proc = QueryProcessor(queries, corpus)
+    
+    # Menjalankan proses pencarian query
+    results = proc.run()
+    
+    qid = 0  # ID untuk query
+    for result in results:
+        # Mengurutkan hasil pencarian berdasarkan nilai (dalam urutan menurun)
+        sorted_x = sorted(result.items(), key=operator.itemgetter(1))
+        sorted_x.reverse()
+        
+        index = 1  # Index untuk ranking hasil pencarian
+        
+        # Mengambil 100 hasil pencarian teratas
+        for i in sorted_x[:100]:
+            tmp = (qid, i[0], index, i[1])
+            # Menampilkan hasil pencarian dengan format yang ditentukan
+            print('{:>1}\tQ0\t{:>4}\t{:>2}\t{:>12}\tNH-BM25'.format(*tmp))
+            index += 1
+            
+        
+        qid += 1  # Menaikkan ID query untuk query selanjutnya
+
+   
+        
 
 if __name__ == '__main__':
     main()
